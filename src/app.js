1242dd56ed89b27149522300bdb66fac9af84b94
Maram Khan <120726987+FaizulMaram@users.noreply.github.com>
2026-05-20 17:01:14 +0500

    Music files added

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const authRoute = require('./routes/auth.route');
+const musicRoute = require('./routes/music.route');
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-    console.log("HOME ROUTE HIT");
-    res.send("Backend Working");
-});
-
 app.use("/api/auth", authRoute);
+app.use("/api/music", musicRoute);
 module.exports = app;
