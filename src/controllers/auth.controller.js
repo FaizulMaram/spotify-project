b50f219fa6a4492560c3840661319cc136ce1da2
Maram Khan <120726987+FaizulMaram@users.noreply.github.com>
2026-05-20 16:29:44 +0500

    login added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -65,6 +65,24 @@ async function loginUser(req, res) {
     if (!isPasswordValid) {
         return res.status(401).json({ message: 'Invalid password' });
     }
+    const token = jwt.sign(
+        {
+            id: user._id,
+            role: user.role
+        }
+        , process.env.JWT_SECRET,
+    )
+
+    res.cookie('token', token);
+
+    res.status(200).json({
+        message: 'Login successful', user: {
+            id: user._id,
+            userName: user.userName,
+            email: user.email,
+            role: user.role
+        }
+    });
 }
 
 module.exports = {
